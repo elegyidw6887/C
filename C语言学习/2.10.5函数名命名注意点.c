fc27fe74d763e684cc04654d9697aee58d6d8c72
LOST <654725797@qq.com>
2020-12-05 00:59:10 +0800

    2020.12.5 0:59

--- C语言学习/2.10.5函数名命名注意点.c
+++ C语言学习/2.10.5函数名命名注意点.c
@@ -2,14 +2,14 @@
 函数名命名注意点
 */
 
-//#include<stdio.h>
-//#include<stdlib.h>
-//
-//int main(void)
-//{
-//
-//
-//
-//	system("pause");
-//	return;
-//}
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+
+
+
+	system("pause");
+	return;
+}
