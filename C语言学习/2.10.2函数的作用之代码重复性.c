6042f1b16b5ee988f0cf64375b52a9bbdd8f81be
LOST <654725797@qq.com>
2020-12-05 00:57:38 +0800

    2020.12.5 0:57

---
+++ C语言学习/2.10.2函数的作用之代码重复性.c
@@ -0,0 +1,33 @@
+/*
+函数的作用之代码的重复性（函数的第一个作用）
+*/
+
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int fun(int* a, int nLength)//自建函数
+//{
+//	int i;
+//	int MAX = a[0];
+//
+//	for (i = 0; i < nLength; i++)
+//	{
+//		if (MAX < a[i])
+//		{
+//			MAX = a[i];
+//		}
+//	}
+//
+//	return MAX;
+//}
+//
+//int main(void)
+//{
+//	int a[5] = { 92,34,265,32,12 };
+//
+//	printf("%d\n", fun(a, 5));//自建函数的调用
+//
+//
+//	system("pause");
+//	return 0;
+//}
---
+++ C语言学习/2.10.5函数名命名注意点.c
@@ -0,0 +1,15 @@
+/*
+函数名命名注意点
+*/
+
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main(void)
+//{
+//
+//
+//
+//	system("pause");
+//	return;
+//}
