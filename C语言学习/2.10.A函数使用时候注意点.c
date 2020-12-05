b7951596f23e89d57e881c602ca8aa707d282a79
LOST <654725797@qq.com>
2020-12-05 12:47:59 +0800

    2020.12.5 12:47

--- C语言学习/2.10.A函数使用时候注意点.c
+++ C语言学习/2.10.A函数使用时候注意点.c
@@ -2,19 +2,21 @@
 函数使用时候的注意点
 */
 
-#include<stdio.h>
-#include<stdlib.h>
-
-int main(void)
-{
-	/*
-	
-	函数调用注意点：
-		1.互相调用死循环的产生
-	*/
-
-
-
-	system("pause");
-	return 0;
-}
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main(void)
+//{
+//	/*
+//	
+//	函数调用注意点：
+//		1.互相调用死循环的产生，或者一个函数体内对自己进行调用
+//
+//		2.调用一个只进行了定义，但是无实体的函数时，就会提示【无法解析的外部命令】
+//	*/
+//
+//
+//
+//	system("pause");
+//	return 0;
+//}
