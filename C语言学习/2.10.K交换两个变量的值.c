8ae43101f756ec304ccd166a7bc0c27eadb78af3
LOST <654725797@qq.com>
2020-12-08 14:14:27 +0800

    2020.12.8 14:14

---
+++ C语言学习/2.10.K交换两个变量的值.c
@@ -0,0 +1,35 @@
+/*
+交换两个变量的值
+*/
+
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//void _ExchangeNum(int* n1, int* n2);
+////函数头的声明
+//
+//int main(void)
+//{
+//	int a = 12,
+//		b = 21;
+//
+//	printf("a = %d,b = %d\n", a, b);//打印执行前的数值
+//	_ExchangeNum(&a, &b);
+//	printf("a = %d,b = %d\n", a, b);//打印执行后的数值
+//
+//
+//	system("pause");
+//	return 0;
+//}
+//
+//void _ExchangeNum(int* n1, int* n2)
+//{
+//
+//	int Ex;//通过一个中间变量来存储数值，从而达到互换数值的目的
+//	Ex = *n1;
+//	*n1 = *n2;
+//	*n2 = Ex;
+//	/*
+//	使用的算法为最基础的数值互换算法
+//	*/
+//}
