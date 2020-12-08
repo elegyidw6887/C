462ecb2b4aae1643c70539c06398e4a5747b0361
LOST <654725797@qq.com>
2020-12-08 23:14:18 +0800

    2020.12.8 23:14

---
+++ C语言学习/2.10.N二维数组做参数.c
@@ -0,0 +1,18 @@
+/*
+二维数组做参数
+*/
+
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main(void)
+//{
+//	/*
+//	
+//	二维数组相关内容学习不扎实，待复习巩固后再学习
+//	*/
+//
+//
+//	system("pause");
+//	return 0;
+//}
