04b7d0367c2eb890c45caa8871ed8d3b86329487
LOST <654725797@qq.com>
2020-12-16 22:49:51 +0800

    2020.12.16 22:49

---
+++ C语言学习/2.11.3char的大小.c
@@ -0,0 +1,26 @@
+/*
+char的大小
+*/
+
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main(void)
+//{
+//	/*
+//	
+//	char变量的大小：
+//		char变量为一个字符，因此大小也为一个字符，sizeof取值之后为1
+//
+//	char变量的表示范围：
+//		有符号：-128~127
+//		无符号：0~255
+//		共计256个数字
+//	*/
+//
+//	char c1 = 'A';
+//	printf("%u\n", sizeof(char));//sizeof取的数值为一个非负数值，但是%d打印的为有符号的十进制数，因此此处使用%u（unsigned int）更合适
+//
+//	system("pause");
+//	return 0;
+//}
