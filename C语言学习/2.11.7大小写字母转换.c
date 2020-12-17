379d6fa88874a044ac1c5d63fd183ab591d11faf
LOST <654725797@qq.com>
2020-12-18 00:21:00 +0800

    2020.12.18 0:20

---
+++ C语言学习/2.11.7大小写字母转换.c
@@ -0,0 +1,38 @@
+/*
+大小写字母的转换
+*/
+
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main(void)
+//{
+//	/*
+//	
+//	大小写字母转换
+//	*/
+//	char c1 = 0;
+//	while (1)//while死循环，使用break代码跳出
+//	{
+//
+//		printf("请输入一个大写或者小写字母，系统会进行大小写转换！\n");
+//		scanf("%c", &c1);
+//		if (c1 >= 65 && c1 <= 90)
+//		{
+//			printf("%c\n", c1 + 32);
+//		}
+//		else if (c1 >= 97 && c1 <= 122)
+//		{
+//			printf("%c\n", c1 - 32);
+//		}
+//		else if ('\n' == c1)
+//		{
+//			break;
+//		}
+//	}
+//
+//
+//	system("pause");
+//	return 0;
+//}
