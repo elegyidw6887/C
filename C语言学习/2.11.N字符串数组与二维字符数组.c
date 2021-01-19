da7cf2aa4eb5eac5b48e9fc295ed8f8e0f13e979
LOST <654725797@qq.com>
2021-01-19 16:46:23 +0800

    2021.1.19 16:46

---
+++ C语言学习/2.11.N字符串数组与二维字符数组.c
@@ -0,0 +1,24 @@
+/*
+字符串数组与二维字符数组
+*/
+
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<string.h>
+//
+//int main(void)
+//{
+//	/*
+//	
+//	声明一个字符串数组类型的指针，指针的每个地址都可以存储一个字符串
+//	*/
+//	char* str1[3] = { "ABC","DEF","GHI" };
+//	printf("%s\n", str1[0]);
+//	printf("%s\n", str1[1]);
+//	printf("%s\n", str1[2]);
+//
+//
+//
+//	system("pause");
+//	return 0;
+//}
