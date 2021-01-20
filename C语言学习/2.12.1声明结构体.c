28d7e0a0a9f2384aa7012872a0dc9cd987f7c270
LOST <654725797@qq.com>
2021-01-20 15:59:05 +0800

    2021.1.20 15:58

---
+++ C语言学习/2.12.1声明结构体.c
@@ -0,0 +1,40 @@
+/*
+声明结构体
+*/
+
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//struct StuInfo
+//{
+//	char Name[10];
+//	unsigned int Age;
+//	double High;
+//	char StuNum[20];
+//};
+//
+//int main(void)
+//{
+//	/*
+//	
+//	结构体
+//		1.结构体的概念：
+//			一个集多种数据类型于一身的构造类型，可以存储多种类型的数据
+//
+//		2.声明一个结构体：
+//			struct Stu
+//			{
+//				char Name[10];
+//				unsigned int Age;
+//				double High;
+//				char StuNum[20];
+//			};
+//			以上就是一个基础结构体的声明，结构体声明的位置是在主函数之前，在此位置（全局区）声明结构体之后，其后的任何函数都可以调用本函数体
+//			其中的各个变量为结构体成员，而且如果一个结构体没有成员则会报错
+//			函数体最后以分号为结尾
+//	*/
+//
+//
+//	system("pause");
+//	return 0;
+//}
