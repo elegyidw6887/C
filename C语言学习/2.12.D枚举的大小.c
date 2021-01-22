bf5604cbab7bc23aee94068deebc36e9045e7d09
LOST <654725797@qq.com>
2021-01-22 22:41:01 +0800

    2021.1.22 22:40

---
+++ C语言学习/2.12.D枚举的大小.c
@@ -0,0 +1,31 @@
+/*
+枚举的大小
+*/
+
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//enum Color { enum_Red = 10, enum_Yellow = 20, enum_Blue = 30, enum_Green = 40 };
+//
+//int main(void)
+//{
+//	/*
+//	
+//	声明一个枚举变量
+//		在枚举声明好之后，与结构体变量相类似的声明方式
+//		同时声明的枚举变量可以赋值任何数值，与一个int类型的变量相同
+//		但是推荐赋值为枚举中声明的成员，以发挥枚举的作用
+//	*/
+//	enum Color co1 = enum_Red;
+//	printf("%d\n", co1);
+//
+//	/*
+//	
+//	枚举的大小：
+//		由于其本质就是int类型的变量，所以大小就是4字节
+//	*/
+//
+//
+//	system("pause");
+//	return 0;
+//}
