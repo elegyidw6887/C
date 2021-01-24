a74a61e24af6770c31e8d948a83a87c2c18ad79e
LOST <654725797@qq.com>
2021-01-24 17:05:03 +0800

    2021.1.26 17:04

---
+++ C语言学习/2.14.1多文件p1.c
@@ -0,0 +1,13 @@
+#include"2.14.1¶àÎÄ¼şp1.h"
+
+int SUM(int a, int b)
+{
+
+	return(a + b);
+}
+
+int MUL(int a, int b)
+{
+
+	return(a * b);
+}
---
+++ C语言学习/2.14.1多文件p1.h
@@ -0,0 +1,2 @@
+int SUM(int a, int b);
+int MUL(int a, int b);
