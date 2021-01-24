a230d34ca29549ec3b1e0a851410d4ab2467094c
LOST <654725797@qq.com>
2021-01-24 17:06:24 +0800

    2021.1.24 17:06

--- C语言学习/2.14.2include的作用.c
+++ C语言学习/2.14.2include的作用.c
@@ -2,20 +2,20 @@
 include的作用
 */
 
-#include<stdio.h>
-#include<stdlib.h>
-
-int main(void)
-{
-	/*
-	
-	include：
-		与define相同，均为预编译指令，均为在程序编译的过程中起作用（以#开头的均为预处理指令）
-		同样的，与define相同，include的功能也是单纯的替换，一句include语句就是将.h中的所有内容替换过来
-	
-	*/
-
-
-	system("pause");
-	return 0;
-}
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main(void)
+//{
+//	/*
+//	
+//	include：
+//		与define相同，均为预编译指令，均为在程序编译的过程中起作用（以#开头的均为预处理指令）
+//		同样的，与define相同，include的功能也是单纯的替换，一句include语句就是将.h中的所有内容替换过来
+//	
+//	*/
+//
+//
+//	system("pause");
+//	return 0;
+//}
