85a88c6c3604706cada30ae949d5b80ac20d887f
LOST <654725797@qq.com>
2021-01-27 10:50:50 +0800

    2021.1.27 10:50

--- C语言学习/2.14.E全局变量与多文件1.c
+++ C语言学习/2.14.E全局变量与多文件1.c
@@ -1,7 +1,7 @@
-#include"2.14.0Common.h"
-
-void Fun1(void)
-{
-	printf("%d\n", e);
-	e = 25;
-}
+//#include"2.14.0Common.h"
+//
+//void Fun1(void)
+//{
+//	printf("%d\n", e);
+//	e = 25;
+//}
--- C语言学习/2.14.E全局变量与多文件2.c
+++ C语言学习/2.14.E全局变量与多文件2.c
@@ -1,7 +1,7 @@
-#include"2.14.0Common.h"
-
-void Fun2(void)
-{
-	printf("%d\n", e);
-	e = 23;
-}
+//#include"2.14.0Common.h"
+//
+//void Fun2(void)
+//{
+//	printf("%d\n", e);
+//	e = 23;
+//}
