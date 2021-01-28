0791965e1e17fb7f6e1092bf206092502757fe71
LOST <654725797@qq.com>
2021-01-28 12:40:13 +0800

    2021.1.28 12:40

---
+++ C语言学习/2.15.5按位或、按位与.c
@@ -0,0 +1,29 @@
+/*
+按位或、按位与
+*/
+
+//#include"Common.h"
+//
+//int main(void)
+//{
+//	/*
+//	
+//	按位与：
+//		有0为0，双1则1
+//
+//	按位或：
+//		有1为1，双0则0
+//	
+//	*/
+//	char a = 79;
+//	char b = -23;
+//	char c = a & b;
+//	printf("%d\n", c);
+//
+//	char d = a | b;
+//	printf("%d\n", d);
+//
+//
+//	system("pause");
+//	return 0;
+//}
